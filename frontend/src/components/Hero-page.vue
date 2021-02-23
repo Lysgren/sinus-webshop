4ccf5829f179d33b9735157767ec7696e50a9b85
Malin S <malin.soderstrom@iths.se>
2021-02-23 15:58:01 +0100

    first styling hero

--- frontend/src/components/Hero-page.vue
+++ frontend/src/components/Hero-page.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>Hero-page</div>
+  <div class="hero-wrapper">
+    <div class="image">
+      <img src="@/assets/hoodie-ocean.png" alt="">
+    </div>
+    <div class="info-text">
+      <h1>HOODIES</h1>
+      <p>499SEK</p>
+      <button>BUY NOW</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +18,52 @@ export default {
 </script>
 
 <style scoped>
+
+*{
+  margin: 0;
+  box-sizing: border-box;
+}
+
+.hero-wrapper{
+  width: 90vw;
+  height: 40vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 5em;
+  background-image: linear-gradient(180deg, #4D566D, white);
+  color: white;
+}
+
+.info-text{
+  margin-right: 18em;
+}
+
+.image{
+  width: 100%;
+  height: 100%;
+  display: flex;
+  
+}
+
+img{
+  height: 100%;
+  width: auto;
+}
+
+h1{
+  font-size: 35px;
+  font-weight: 400;
+}
+
+button{
+  margin-top: 3em;
+  border: none;
+  background-color: #EFF31E;
+  color: black;
+  font-weight: 700;
+  padding: 0.5em 1em;
+  cursor: pointer;
+}
+
 </style>
