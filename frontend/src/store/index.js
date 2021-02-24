c277e56e14e91776061f56ebb2146eb178727253
hanna <hanna.aspblad@iths.se>
2021-02-24 10:53:09 +0100

    la moduler i mapp

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 
-import customerModule from '@/store/customer.store.js'
-import ordersModule from '@/store/orders.store.js'
-import productsModule from '@/store/products.store.js'
+import customerModule from '@/store/modules/customer.store.js'
+import ordersModule from '@/store/modules/orders.store.js'
+import productsModule from '@/store/modules/products.store.js'
 
 Vue.use(Vuex)
 
