da702c5d5923b890ddb91db44216a0fbd3370cf0
Malin S <malin.soderstrom@iths.se>
2021-02-24 11:49:32 +0100

    new component product-details, added to productPage

--- frontend/src/views/productPage.vue
+++ frontend/src/views/productPage.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="home">
     <Navbar />
-    <div></div>
-    <otherMAL />
+    <div class="home-wrapper">
+      <div class="dao">
+          <Product-details :product="currentProduct"/>
+          <OtherMAL />
+      </div>
+    </div>    
     <Footer />
   </div>
 </template>
@@ -11,8 +15,41 @@
 import Navbar from '@/components/Navbar.vue'
 import OtherMAL from '@/components/otherMayAlsoLike.vue'
 import Footer from '@/components/Footer.vue'
+import ProductDetails from '../components/Product-details.vue'
 
 export default {
- components: { Navbar, OtherMAL, Footer }
+  components: { Navbar, OtherMAL, Footer, ProductDetails },
+  data(){return{
+   currentProduct: {
+          id: 13434,
+          item: "hoodie",
+          price: "499",
+          img: "'@/assets/hoodie-ocean.png'",
+          color: "ocean"
+    }
+  }}
 }
 </script>
+
+<style scoped>
+
+.home {
+  background-color: #DCDCDC;
+}
+
+.home-wrapper{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* margin: 4em 0; */
+}
+
+.dao{
+  width: 90vw;
+  height: auto;
+}
+
+
+
+</style>
