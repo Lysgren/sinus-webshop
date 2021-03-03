db8388bed2da7dc01fc05b510ee6b4f35d231a58
Lysse <Oscar.Lysgren@gmail.com>
2021-03-03 12:00:25 +0100

    Design done

--- frontend/src/components/login/register.vue
+++ frontend/src/components/login/register.vue
@@ -1,21 +1,24 @@
 <template>
   <div class="login-user">
     <h1>Register account</h1>
-    <form @submit.prevent="registerUser" class="login-user-inputs">
+    <form @submit.prevent="registerUser" class="login-user-inputs" :class="{ error: error }">
       <label for="mail">Email</label>
-      <input name="mail" v-model="newUser.email" type="email" placeholder="email" />
+      <input name="mail" v-model="newUser.email" type="email" placeholder="email" required/>
       <label for="password">Password</label>
-      <input name="password" v-model="newUser.password" type="text" placeholder="password" />
+      <input name="password" v-model="newUser.password" type="text" placeholder="password" required/>
       <label for="name">Name</label>
-      <input name="name" v-model="newUser.name" type="text" placeholder="name" />
+      <input name="name" v-model="newUser.name" type="text" placeholder="name" required/>
       <label for="street">Street</label>
-      <input name="street" v-model="newUser.address.street" type="text" placeholder="street" />
+      <input name="street" v-model="newUser.address.street" type="text" placeholder="street" required/>
       <label for="zip" >Zip</label>
-      <input name="zip" v-model="newUser.address.zip" type="text" placeholder="zip" />
+      <input name="zip" v-model="newUser.address.zip" type="text" placeholder="zip" required/>
       <label for="city">City</label>
-      <input name="city" v-model="newUser.address.city" type="text" placeholder="city" />
-      <button>Registrera</button>
+      <input name="city" v-model="newUser.address.city" type="text" placeholder="city" required/>
+      <span v-if="error == true">Information entered incorrectly...</span>
     </form>
+    <div class="button-container">
+      <button v-on:click="registerUser">Registrera</button>
+    </div>
   </div>
 </template>
 <script>
@@ -35,16 +38,33 @@ export default {
     };
   },
   methods: {
-    registerUser() {
-      const payload = this.newUser;
-      this.$store.dispatch("registerUser", payload);
-      this.$emit("clicked", 'login');
+    async registerUser() {
+      if (this.newUser.email != '' && this.newUser.password != '' && this.newUser.name != '' && this.newUser.address.street != '' && this.newUser.address.zip != '' && this.newUser.address.city != '') {
+        const payload = this.newUser
+        const value = await this.$store.dispatch("registerUser", payload)
+        if (value == true) {
+          this.$emit("clicked", 'login')
+        }
+      }
     }
-  }
-};
+  },
+  computed: {
+    error() {
+      return this.$store.getters.getError;
+    }
+  } 
+}
 </script>
 
 <style lang="scss" scoped>
+.error {
+  label,
+  button,
+  input {
+    color: red;
+  }
+}
+
 .login-user-inputs {
   display: grid;
   grid-template-columns: repeat(2, 1fr);
@@ -59,4 +79,12 @@ export default {
     padding: 0.24rem;
   }
 }
+
+.button-container {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
+.button-container button{ width: 100%; }
+
 </style>
