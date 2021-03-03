e9edd963c2ad3f06c40d4e18a450302ca259e61c
Phnome <soold@hotmail.se>
2021-03-03 11:43:06 +0100

    'editMode'

--- frontend/src/views/editProfile.vue
+++ frontend/src/views/editProfile.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="login-user">
     <form @submit.prevent="updateUser" class="login-user-inputs">
-      <input :value="userData.email" v-on:input="editUser.email = $event.target.value" type="email" placeholder="mail"/>
-      <input :value="userData.name" v-on:input="editUser.name = $event.target.value" type="text" placeholder="name" />
-      <input :value="userData.address.street" v-on:input="editUser.address.street = $event.target.value" type="text" placeholder="street"/>
-      <input :value="userData.address.zip" v-on:input="editUser.address.zip = $event.target.value" type="text" placeholder="zip" />
-      <input :value="userData.address.city" v-on:input="editUser.address.city = $event.target.value" type="text" placeholder="city" />
+      <label for="mail">Email</label>
+      <input
+        v-model="editUser.email"
+        name="mail"
+        type="email"
+        placeholder="mail"
+      />
+      <label for="name">Name</label>
+      <input
+        name="name"
+        v-model="editUser.name"
+        type="text"
+        placeholder="name"
+      />
+      <label for="street">Street</label>
+      <input
+        name="street"
+        v-model="editUser.address.street"
+        type="text"
+        placeholder="street"
+      />
+      <label for="zip">Zip</label>
+      <input
+        name="zip"
+        v-model="editUser.address.zip"
+        type="text"
+        placeholder="zip"
+      />
+      <label for="city">City</label>
+      <input
+        name="city"
+        v-model="editUser.address.city"
+        type="text"
+        placeholder="city"
+      />
       <button>Uppdatera</button>
     </form>
   </div>
@@ -37,29 +67,43 @@ export default {
   computed: {
     userData() {
       return this.$store.getters.getUserData;
-    }
+    },
   },
-  mounted(){
-    if (!this.$store.getters.getUserToken || !this.$store.getters.getAdmin) {
-      this.$router.push("/")
+  mounted() {
+    if (!this.$store.getters.getUserToken) {
+      this.$router.push("/");
     }
+    this.editUser = this.userData;
   },
 };
 </script>
 
 <style lang="scss" scoped >
-.login-user-inputs {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-auto-flow: row;
-  grid-auto-rows: auto;
-  gap: 1rem;
-  min-width: 24rem;
-  padding: 0.24rem;
-  height: 100%;
-  width: 100%;
-  label {
-    padding: 0.24rem;
+.login-user {
+  min-height: 60vh;
+  height: fit-content;
+  padding: 6.4rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #dcdcdc;
+  .login-user-inputs {
+    background: white;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-auto-flow: row;
+    grid-auto-rows: auto;
+    gap: 1rem;
+    min-width: 24rem;
+    padding: 1.28rem;
+    height: 100%;
+    width: 100%;
+    label {
+      padding: 0.24rem;
+    }
+    input {
+      width: 100%;
+    }
   }
 }
 </style>
